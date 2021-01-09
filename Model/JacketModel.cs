a7b552802e740c8eeca4840c434ac39778380074
Mohamed Farhat <mohamedfarhat01@gmail.com>
2021-01-09 17:53:22 +0400

    Add project files.

---
+++ Model/JacketModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MyShoppingAppBackend.Model
+{
+    public partial class JacketModel
+    {
+        public JacketModel()
+        {
+            ProductJacketModels = new HashSet<ProductJacketModel>();
+        }
+
+        public int JacketModelId { get; set; }
+        public string JacketModelName { get; set; }
+
+        public virtual ICollection<ProductJacketModel> ProductJacketModels { get; set; }
+    }
+}
